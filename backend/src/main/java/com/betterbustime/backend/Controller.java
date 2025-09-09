026604b8cc8eb42443253b47b147dce8446d1600
Mark <208412620+Markyriott@users.noreply.github.com>
2025-09-09 09:55:13 -0400

    Redis config

--- backend/src/main/java/com/betterbustime/backend/Controller.java
+++ backend/src/main/java/com/betterbustime/backend/Controller.java
@@ -1,26 +1,15 @@
 package com.betterbustime.backend;
 
-import org.springframework.data.redis.connection.RedisConnection;
-import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.serializer.GenericJackson3JsonRedisSerializer;
-import org.springframework.data.redis.serializer.StringRedisSerializer;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Controller {
 
-    RedisConnectionFactory redisConnectionFactory;
+    @Autowired
     RedisTemplate<String, Object> redisTemplate;
-
-    public Controller(RedisConnectionFactory redisConnectionFactory) {
-        this.redisConnectionFactory = redisConnectionFactory;
-        this.redisTemplate = new RedisTemplate<>();
-        this.redisTemplate.setConnectionFactory(redisConnectionFactory);
-        this.redisTemplate.setKeySerializer(new StringRedisSerializer());
-        this.redisTemplate.afterPropertiesSet();
-    }
 
     @GetMapping("/bus")
     public String bus(){
